909ee5c8794053def53408bffb552a37b428c514
Aman Srivastav <shinchanmusic24@gmail.com>
2023-10-15 14:00:57 +0530

    useParams

--- currencyConverter/src/main.jsx
+++ currencyConverter/src/main.jsx
@@ -6,17 +6,28 @@ import {RouterProvider,createBrowserRouter} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
     children: [{
-      path:"home",
+      path:"",
       element:<Home/>
     },{
       path:"about",
       element : <About/>
-    }]
+    }
+    ,{
+      path:"contact",
+      element : <Contact/>
+    }
+    ,{
+      path:"user/:userid",
+      element : <User/>
+    }
+  ]
   }
 ])
 
